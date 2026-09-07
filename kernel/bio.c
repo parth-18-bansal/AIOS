76bd878f3056de6bd60a4b53fa3a1ce2e19fa158
Parth Bansal <parthbansalcoding@gmail.com>
2026-09-07 22:57:57 +0530

    buffer cache init

--- kernel/bio.c
+++ kernel/bio.c
@@ -1,9 +1,43 @@
+#include <spinlock.h>
+#include <buf.h>
+#include <param.h>
+
+
 /*
-summary:
+summary: this is the buffer cache
 */
 struct{
+    struct spinlock lock;
+    struct buf buf[NBUF];
+    
+    struct buf head;
 
 } bcache;
+
+/*
+summary:
+here we are creating a linkedlist of buffers
+
+head --> n --> n-1 --> ... --> 1 --> head
+*/
+void binit(void){
+    struct buf *b;
+
+    initlock(&bcache.lock, "bcache");
+
+    bcache.head.prev = &bcache.head;
+    bcache.head.next = &bcache.head;
+
+    for(b = bcache.buf; b < bcache.buf + NBUF; b++){
+        b->next = bcache.head.next;
+        b->prev = &bcache.head;
+
+        initsleeplock(&b->lock, "buffer");
+
+        bcache.head.next->prev = b;
+        bcache.head.next = b;
+    }
+}
 
 /*
 summary:
--- kernel/param.h
+++ kernel/param.h
@@ -3,3 +3,6 @@
 #define NCPU 8    // number of cpus
 
 #define NINODE 50 // maximum number of active inodes that can be cached in the RAM at a time
+
+#define MAXOPBLOCKS 10
+#define NBUF (MAXOPBLOCKS * 3) // SIZE OF THE buf cache
